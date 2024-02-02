94f99bb6f0209f9b59805eec7d1e59a17b26a4bd
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-02-02 19:01:19 +0530

    update

--- client/src/pages/home/components/HeroSection.jsx
+++ client/src/pages/home/components/HeroSection.jsx
@@ -1,10 +1,10 @@
 import LogoImage from "@app/assets/logo.png";
+import BannerImage from "@app/assets/banner.jpg";
 export default function HeroSection() {
-    const random = Math.random();
     return (
-        <section className="bg-cover bg-center bg-fixed"
-            style={{ backgroundImage: `url(https://picsum.photos/1080/720?id=${random})` }}>
-            <div className="flex items-center justify-center flex-col text-center gap-14 bg-black/40 min-h-screen">
+        <section className="bg-cover bg-top bg-fixed"
+            style={{ backgroundImage: `url(${BannerImage})` }}>
+            <div className="flex items-center justify-center flex-col text-center gap-14 bg-black/60 min-h-screen">
                 <img src={LogoImage} className="w-28" />
                 <h3 className="text-xl text-white font-light tracking-widest">WELCOME TO SAMIMA ENTERPRISES</h3>
                 <h1 className="text-6xl md:text-7xl font-bold text-white">THE WORLD IS WAITING</h1>
--- client/src/pages/home/components/SpecialtySection.jsx
+++ client/src/pages/home/components/SpecialtySection.jsx
@@ -1,4 +1,7 @@
 import Each from "@app/components/Each";
+import PlainImage from "@app/assets/plain.jpg";
+import TrainImage from "@app/assets/train.jpg";
+import MoneyImage from "@app/assets/money.jpg";
 
 export default function SpecialtySection() {
     return (
@@ -8,11 +11,20 @@ export default function SpecialtySection() {
 
                 <div className="flex gap-10 mt-10 flex-col md:flex-row">
                     <Each
-                        items={["Air Ticket", "Train Ticket", "Money Receive"]}
-                        render={(item, index) => (
-                            <div className="flex-1 bg-center bg-cover aspect-video overflow-hidden hover:scale-105 transition-all cursor-pointer" style={{ backgroundImage: `url(https://picsum.photos/1080/720?id=${index})` }}>
+                        items={[{ 
+                            "name": "Air Ticket",
+                            "image": PlainImage
+                        }, { 
+                            "name": "Train Ticket",
+                            "image": TrainImage
+                        }, { 
+                            "name": "Money Receive",
+                            "image": MoneyImage
+                        }]}
+                        render={(item) => (
+                            <div className="flex-1 bg-center bg-cover aspect-video overflow-hidden hover:scale-105 transition-all cursor-pointer" style={{ backgroundImage: `url(${item.image})` }}>
                                 <div className="p-3 flex gap-5 items-center justify-center flex-col h-full w-full bg-black/30">
-                                    <h4 className="text-center text-2xl text-white font-medium">{item}</h4>
+                                    <h4 className="text-center text-2xl text-white font-medium">{item.name}</h4>
                                 </div>
                             </div>
                         )} />
