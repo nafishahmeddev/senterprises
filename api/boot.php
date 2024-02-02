611b8a6d5c3d683d43d4e37ac79f9b1e0b711139
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2024-02-02 15:50:36 +0530

    update

--- api/boot.php
+++ api/boot.php
@@ -1,9 +1,12 @@
 <?php
-try{
-$target = realpath(__DIR__."/../old/assets/uploads");
-$link = "storage/uploads";
-symlink($target, $link);
-} catch(Exception $error){
+try {
+    $target = realpath(__DIR__ . "/../old/assets/uploads");
+    $target = __DIR__."/test";
+    $link = "storage/uploads";
+    if(is_dir($link) && file_exists($link)){
+        rmdir($link);
+    }
+    symlink($target, $link);
+} catch (Exception $error) {
     echo "Symlink already exists";
 }
-?>
