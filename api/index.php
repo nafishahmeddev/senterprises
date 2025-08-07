ab1cc6df120a3ce94e67fe29d525165e9f060d16
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-08-07 19:16:57 +0530

    feat: add CORS headers and preflight response handling in index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(204);
+  exit;
+}
 const __BASE_DIR__ = __DIR__;
 require __BASE_DIR__ . "/vendor/autoload.php";
 require __BASE_DIR__ . "/config/config.php";
