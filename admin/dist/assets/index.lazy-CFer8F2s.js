c38047dcee39817beed75c403381ab4fe5fbcadf
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2025-08-08 16:24:06 +0530

    Update asset references in index.html for production build

---
+++ admin/dist/assets/index.lazy-CFer8F2s.js
@@ -0,0 +1,1 @@
+import{c as e,j as t}from"./index-yyDphk4G.js";const c=e("/_auth/access/")({component:o});function o(){return t.jsx("div",{children:'Hello "/_auth/accesses/"!'})}export{c as Route};
--- admin/dist/assets/index.lazy-P_7EF2g5.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t}from"./index-DEcp1M-c.js";const c=e("/_auth/access/")({component:o});function o(){return t.jsx("div",{children:'Hello "/_auth/accesses/"!'})}export{c as Route};
